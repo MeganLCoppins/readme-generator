3718efd82ec0c1fe815af6e64de3af0480188228
Megan Coppins <meganlcoppins@gmail.com>
2020-02-27 09:40:35 -0700

    require commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const api = require("./utils/api");
 const questions = require("./utils/questions");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // function writeToFile(functionName, data) {}
 
@@ -13,7 +14,7 @@ async function init() {
 //   next two lines break down what const {data : gitInfo}
 //   const data = await api.getUser(userInfo.username);
 //   const gitInfo = data.data;
-  readme = createReadme(userInput, gitInfo)
+  readme = generateMarkdown(userInput, gitInfo)
 
 //create md file with information returned from createReadme function
   fs.writeFile("readMe.md", readme, function(err){
@@ -22,59 +23,5 @@ async function init() {
       }
   })
 };
-
-// create readme based on user input and github info
-function createReadme(userInput, gitInfo){
-// if user has an email address than email will be presented otherwise "user does not have an email" will be displayed
-    const email = gitInfo.email || "User does not have an email"
-
-    return `
-    # ${userInput.title}
-
-    ## Description
-
-    ${userInput.description}
-
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    
-    ## Installation
-
-    ${userInput.installation}
-
-    ## Usage
-
-    ${userInput.usage}
-
-    ## License
-
-    This project is licensed under the ${userInput.license} license.
-
-    <img src="https://img.shields.io/badge/License-${userInput.license}-orange" alt="badge"/>
-
-    ## Contributing
-
-    ${userInput.credits}
-
-    ## Tests
-
-
-        ${userInput.tests}
-
-
-    ## Questions
-
-    For any questions regarding the repo contact: ${email}
-
-    GitHub username: ${gitInfo.login}
-
-    <img src="${gitInfo.avatar_url}" alt="avatar" height="200" width="200"/>
-    `;
-}
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,53 @@
-function generateMarkdown(data) {
+function generateMarkdown(userInput, gitInfo) {
+  // if user has an email address than email will be presented otherwise "user does not have an email" will be displayed
+  const email = gitInfo.email || "User does not have an email"
+
   return `
-# ${data.title}
+  # ${userInput.title}
 
-`;
+  ## Description
+
+  ${userInput.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+
+  ${userInput.installation}
+
+  ## Usage
+
+  ${userInput.usage}
+
+  ## License
+
+  This project is licensed under the ${userInput.license} license.
+
+  <img src="https://img.shields.io/badge/License-${userInput.license}-orange" alt="badge"/>
+
+  ## Contributing
+
+  ${userInput.credits}
+
+  ## Tests
+
+
+      ${userInput.tests}
+
+
+  ## Questions
+
+  For any questions regarding the repo contact: ${email}
+
+  GitHub username: ${gitInfo.login}
+
+  <img src="${gitInfo.avatar_url}" alt="avatar" height="200" width="200"/>
+  `;
 }
-
 module.exports = generateMarkdown;
